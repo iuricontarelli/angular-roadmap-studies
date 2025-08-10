f0ccb430ef6049f41947e7b5f4068c4fd596cbc9
iuricontarelli <1905iuri@gmail.com>
2025-08-10 10:26:17 -0300

    chore: remove arquivo de exercícios de tipos primitivos

--- 01-fundamentos-typescript/01-tipos-primitivos/exercicios.ts
+++
@@ -1,8 +0,0 @@
-// 1. Crie uma variável com o nome do seu framework favorito
-let framework: string = 'Angular';
-
-// 2. Crie uma variável com o ano atual
-let ano: number = 2025;
-
-// 3. Crie uma variável que indique se você está com foco total
-let foco: boolean = true;
